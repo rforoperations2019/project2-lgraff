a22aff578c474025cdbb218d0ea9e32a47a1bbf5
Lindsay Graff <lgraff@andrew.cmu.edu>
2019-10-13 20:58:23 -0400

    Data preparation
    Prepped dataset by: subsetting for just 2016 values, renaming columns, changing column data types, adding lat and long

--- project2_testing.R
+++ project2_testing.R
@@ -1,1 +1,43 @@
-a
+# Load packages
+require(rgdal)
+require(leaflet)
+require(leaflet.extras)
+
+require(dplyr)
+require(readxl)
+require(stringr)
+
+require(raptr)
+require(PBSmapping)
+
+# Load FL eviction data. Source: https://data-downloads.evictionlab.org/
+FL_counties <- readOGR("FL_eviction_counties.geojson")
+View(FL_counties@data)
+FL_16 <- FL_counties
+
+# Select only 2016 data and rename columns
+FL_16@data <- FL_counties@data %>% 
+  dplyr::select(GEOID, west, east, north, south, n, pl, ends_with("16")) %>% 
+  rename(county = n, state = pl)
+View(FL_16@data)
+colnames(FL_16@data) <- c("GEOID", "west", "east", "north", "south", "county", "state",
+                          "population", "poverty_rate", "renter_occ_households", "pct_renter",
+                          "median_gross_rent", "median_income", "median_property_val",
+                          "rent_burden", "pct_white", "pct_af_am", "pct_hispanic", "pct_am_ind",
+                          "pct_asian", "pct_nh", "pct_multiple", "pct_other", 
+                          "eviction_filings", "evictions", "eviction_rate", "evic_filing_rate",
+                          "low_flag", "imputed", "sub")
+
+# Add lat and long to the dataset
+# Calculate centroids of counties to find lat and long
+FL_polyset <- SpatialPolygons2PolySet(x = FL_16)
+centroid <- calcCentroid(FL_polyset, rollup = 1) %>% 
+  rename(lat = Y, long = X)
+
+# Bind county centroids to Florida data
+FL_16@data <- cbind(FL_16@data, centroid)
+View(FL_16@data)
+
+# Data types
+sapply(FL_16@data, typeof)
+FL_16@data$county <- as.factor(FL_16@data$county)
